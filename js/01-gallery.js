b635efa09f7099a9792c2bef57d48b77b64ae987
Vicars <vicarsic@gmail.com>
2023-03-17 18:59:21 +0200

    111

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+galleryEl = document.querySelector(".gallery");
+galleryItems.forEach((item) =>
+  galleryEl.insertAdjacentHTML(
+    "beforeend",
+    `<div class="gallery__item">
+        <a class="gallery__link" href='${item.original}'>
+            <img class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+        
+    </div>`
+  )
+);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
